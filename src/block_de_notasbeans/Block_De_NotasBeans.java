9290e33b67facb29f55acefb8f313f7aabf2a60a
dam2 <dam2@a223e04.dc.domenicoscarlatti.es>
2024-10-24 16:49:14 +0200

    Creancion del proyecto

---
+++ src/block_de_notasbeans/Block_De_NotasBeans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package block_de_notasbeans;
+
+/**
+ *
+ * @author dam2
+ */
+public class Block_De_NotasBeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
